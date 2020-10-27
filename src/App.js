6436f4f143abdeae54b02b6a929fba9733d73290
Gerrit Degenhardt <gerrit@degenhardt.be>
2020-10-27 14:36:14 +0100

    Add world map

--- src/App.js
+++ src/App.js
@@ -2,11 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 import {VectorMap} from "react-jvectormap"
 
+function MyVerticallyCenteredModal(props) {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Header closeButton >
+                <Modal.Title id="contained-modal-title-vcenter" >
+                    Modal heading
+                </Modal.Title >
+            </Modal.Header >
+            <Modal.Body >
+                <h4 >Centered Modal</h4 >
+                <p >
+                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+                    consectetur ac, vestibulum at eros.
+                </p >
+            </Modal.Body >
+            <Modal.Footer >
+                <Button onClick={props.onHide} >Close</Button >
+            </Modal.Footer >
+        </Modal >
+    );
+}
+
 function App() {
 
     const API = 'http://bc_final-project-api/index.php/';
     const COUNTRY_GET_LIST = 'country/api';
-
+    const COLOR_VISITED = '';
 
     fetch(API + COUNTRY_GET_LIST)
         .then(response => response.json())
@@ -14,22 +42,24 @@ function App() {
 
     const mapData = {
         CN: 100000,
-        IN: 9900,
-        SA: 86,
-        EG: 70,
-        SE: 0,
-        FI: 0,
-        FR: 0,
-        US: 20
+        IN: 0,
+        SA: 0,
+        EG: 50000,
+        SE: 30000,
+        FI: 20000,
+        FR: 10000,
+        US: 0
     };
 
     const handleClick = (e, countryCode) => {
         console.log(countryCode);
+        alert(countryCode);
+
     };
 
     const Map = () => {
         return (
-            <div>
+            <div >
                 <VectorMap
                     map={"world_mill"}
                     backgroundColor="transparent" //change it to ocean blue: #0077be
@@ -68,53 +98,66 @@ function App() {
                         ]
                     }}
                 />
-            </div>
+            </div >
         );
     };
 
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
 
+        <>
+            <Button variant="primary" onClick={() => setModalShow(true)}>
+                Launch vertically centered modal
+            </Button>
 
-    <div>
-        <VectorMap
-            map={"world_mill"}
-            backgroundColor="transparent" //change it to ocean blue: #0077be
-            zoomOnScroll={false}
-            containerStyle={{
-                width: "100%",
-                height: "520px"
-            }}
-            onRegionClick={handleClick} //gets the country code
-            containerClassName="map"
-            regionStyle={{
-                initial: {
-                    fill: "#e4e4e4",
-                    "fill-opacity": 0.9,
-                    stroke: "none",
-                    "stroke-width": 0,
-                    "stroke-opacity": 0
-                },
-                hover: {
-                    "fill-opacity": 0.8,
-                    cursor: "pointer"
-                },
-                selected: {
-                    fill: "#2938bc" //color for the clicked country
-                },
-                selectedHover: {}
-            }}
-            regionsSelectable={true}
-            series={{
-                regions: [
-                    {
-                        values: mapData, //this is your data
-                        scale: ["#146804", "#ff0000"], //your color game's here
-                        normalizeFunction: "polynomial"
-                    }
-                ]
-            }}
-        />
-    </div>
+            <MyVerticallyCenteredModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
+
+            <div >
+                <VectorMap
+                    map={"world_mill"}
+                    backgroundColor="transparent" //change it to ocean blue: #0077be
+                    zoomOnScroll={false}
+                    containerStyle={{
+                        width: "100%",
+                        height: "520px"
+                    }}
+                    onRegionClick={handleClick} //gets the country code
+                    containerClassName="map"
+                    regionStyle={{
+                        initial: {
+                            fill: "#e4e4e4",
+                            "fill-opacity": 0.9,
+                            stroke: "none",
+                            "stroke-width": 0,
+                            "stroke-opacity": 0
+                        },
+                        hover: {
+                            "fill-opacity": 0.8,
+                            cursor: "pointer"
+                        },
+                        selected: {
+                            fill: "#2938bc" //color for the clicked country
+                        },
+                        selectedHover: {}
+                    }}
+                    regionsSelectable={true}
+                    series={{
+                        regions: [
+                            {
+                                values: mapData, //this is your data
+                                scale: ["#146804", "#2938bc"], //your color game's here
+                                normalizeFunction: "polynomial"
+                            }
+                        ]
+                    }}
+                />
+            </div >
+
+        </>
     );
 }
 
